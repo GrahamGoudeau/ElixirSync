acc917898539680bd3ed5c22af7e5c9a785bc5f2
Graham Goudeau <grahamgoudeau@gmail.com>
2016-01-07 21:01:33 -0600

    Rewrote main module with better usage of OptionParser

--- lib/sync.ex
+++ lib/sync.ex
@@ -1,10 +1,28 @@
 defmodule Sync do
   def main(args) do
-    args |> parse_args |> Sync.Sync_Manager.sync
+    args |> parse_args |> process_args
   end
 
-  def parse_args(args) do
-    {_, dirs, _} = OptionParser.parse(args)
-    dirs
+  defp process_args(:help) do
+    IO.puts """
+      Usage:
+      ./sync [dir1] [dir2] [...]
+
+      Description:
+      Concurrently synchronize the contents of two or more directories
+      """
+  end
+
+  defp process_args(dirs) do
+    Sync.Sync_Manager.sync dirs
+  end
+
+  defp parse_args(args) do
+    #{_, dirs, _} = OptionParser.parse(args)
+    case OptionParser.parse(args) do
+      {_, [], _} -> :help
+      {[], [dir1 | [dir2 | dirs]], []} -> [dir1 | [dir2 | dirs]]
+      _ -> :help
+    end
   end
 end
